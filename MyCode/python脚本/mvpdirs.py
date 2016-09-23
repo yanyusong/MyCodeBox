a024f5f070351687015d8e87a625f2795376c4f7
mac <yusongeric@gmail.com>
2016-09-23 09:49:09 +0800

    更新架构文件夹目录生成脚本

--- MyCode/python脚本/mvpdirs.py
+++ MyCode/python脚本/mvpdirs.py
@@ -6,9 +6,11 @@
 # @Version : $Id$
 
 import os
+import os.path
+import shutil
 
-filesTree1 = ('base','common','components','modules')
-filesTree2 = (('adapter','module'),('beans','helpers','widgets','utils'),('http','cache'),())
+filesTree1 = ('base','common','data','modules')
+filesTree2 = (('activity','adapter','module'),('helpers','widgets','utils'),('beans','http','cache'),())
 
 originDir = os.path.abspath('.')
 print('当前目录是－－－'+ originDir)
@@ -18,10 +20,10 @@ print(len(filesTree2))
 if len(filesTree1)!=len(filesTree2):
 	print('error：－－－－filesTree1和filesTree2数量不一样')
 else:
-	i = len(filesTree1)
-	while i > 0:
-		currentDir = os.path.join(originDir,filesTree1[len(filesTree1)-i])
-		os.makedirs(currentDir)
-		for fileDir in filesTree2[len(filesTree1)-i]:
+	for i in range(len(filesTree1)):
+		currentDir = os.path.join(originDir,filesTree1[i])
+		if len(filesTree2[i])==0 :
+			os.makedirs(os.path.join(currentDir,""))
+			continue
+		for fileDir in filesTree2[i]:
 			os.makedirs(os.path.join(currentDir,fileDir))
-		i=i-1
